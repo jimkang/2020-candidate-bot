198984a1e241574da2d74ff4bbf71020673a6cb4
Jim Kang <jimkang@gmail.com>
2018-01-30 09:28:08 -0500

    Post to archive.

--- post-tweet.js
+++ post-tweet.js
@@ -6,6 +6,24 @@ var getCandidatePromo = require('./get-candidate-promo');
 var sb = require('standard-bail')();
 var callNextTick = require('call-next-tick');
 
+var StaticWebArchiveOnGit = require('static-web-archive-on-git');
+var queue = require('d3-queue').queue;
+var randomId = require('idmaker').randomId;
+
+var staticWebStream = StaticWebArchiveOnGit({
+  config: config.github,
+  title: config.archiveName,
+  footerScript: `<script type="text/javascript">
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-49491163-1', 'jimkang.com');
+  ga('send', 'pageview');
+</script>`,
+  maxEntriesPerPage: 50
+});
+
 var dryRun = false;
 if (process.argv.length > 2) {
   dryRun = process.argv[2].toLowerCase() == '--dry';
@@ -19,19 +37,35 @@ var twit = new Twit(config.twitter);
 function run() {
   tries += 1;
 
-  getCandidatePromo(sb(postTweet, wrapUp));
+  getCandidatePromo(sb(postToTargets, wrapUp));
+}
+
+function postToTargets(text, done) {
+  if (dryRun) {
+    console.log('Would have tweeted:', text);
+  } else {
+    var q = queue();
+    q.defer(postTweet, text);
+    q.defer(postToArchive, text);
+    q.await(done);
+  }
 }
 
 function postTweet(text, done) {
-  if (dryRun) {
-    console.log('Would have tweeted:', text);
-    callNextTick(done);
-  } else {
-    var body = {
-      status: text
-    };
-    twit.post('statuses/update', body, done);
-  }
+  var body = {
+    status: text
+  };
+  twit.post('statuses/update', body, done);
+}
+
+function postToArchive(text, done) {
+  var id = 'candidate-' + randomId(8);
+  staticWebStream.write({
+    id,
+    date: new Date().toISOString(),
+    caption: text
+  });
+  staticWebStream.end(done);
 }
 
 function wrapUp(error, data) {
